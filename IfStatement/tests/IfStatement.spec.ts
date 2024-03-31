d07864a5274047691641253d0f65ca53182a0f71
Swafox <swafox@pm.me>
2024-03-31 20:33:37 +0300

    Add If Statement example

---
+++ IfStatement/tests/IfStatement.spec.ts
@@ -0,0 +1,87 @@
+import { Blockchain, SandboxContract, TreasuryContract } from "@ton/sandbox";
+import { Cell, toNano } from "@ton/core";
+import { IfStatement } from "../wrappers/IfStatement";
+import "@ton/test-utils";
+import { compile } from "@ton/blueprint";
+
+describe("IfStatement", () => {
+  let code: Cell;
+
+  beforeAll(async () => {
+    code = await compile("IfStatement");
+  });
+
+  let blockchain: Blockchain;
+  let deployer: SandboxContract<TreasuryContract>;
+  let ifStatement: SandboxContract<IfStatement>;
+
+  beforeEach(async () => {
+    blockchain = await Blockchain.create();
+
+    ifStatement = blockchain.openContract(
+      IfStatement.createFromConfig(
+        {
+          id: Math.floor(Math.random() * 10000),
+          counter: 0,
+        },
+        code,
+      ),
+    );
+
+    deployer = await blockchain.treasury("deployer");
+
+    const deployResult = await ifStatement.sendDeploy(
+      deployer.getSender(),
+      toNano("0.05"),
+    );
+
+    expect(deployResult.transactions).toHaveTransaction({
+      from: deployer.address,
+      to: ifStatement.address,
+      deploy: true,
+      success: true,
+    });
+  });
+
+  it("should deploy", async () => {
+    // the check is done inside beforeEach
+    // blockchain and ifStatement are ready to use
+  });
+
+  it("should increase counter", async () => {
+    const increaseTimes = 3;
+    for (let i = 0; i < increaseTimes; i++) {
+      console.log(`increase ${i + 1}/${increaseTimes}`);
+
+      const increaser = await blockchain.treasury("increaser" + i);
+
+      const counterBefore = await ifStatement.getCounter();
+
+      console.log("counter before increasing", counterBefore);
+
+      const increaseBy = Math.floor(Math.random() * 100);
+
+      console.log("increasing by", increaseBy);
+
+      const increaseResult = await ifStatement.sendIncrease(
+        increaser.getSender(),
+        {
+          increaseBy,
+          value: toNano("0.05"),
+        },
+      );
+
+      expect(increaseResult.transactions).toHaveTransaction({
+        from: increaser.address,
+        to: ifStatement.address,
+        success: true,
+      });
+
+      const counterAfter = await ifStatement.getCounter();
+
+      console.log("counter after increasing", counterAfter);
+
+      expect(counterAfter).toBe(counterBefore + increaseBy);
+    }
+  });
+});
