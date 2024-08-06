3592f66a6fae7e47d4f8b83463b6a99f0743d470
Swafox <swafox@pm.me>
2024-08-06 18:49:12 +0300

    Implement module operations

--- Mathematics/tests/Mathematics.spec.ts
+++ Mathematics/tests/Mathematics.spec.ts
@@ -1,130 +1,152 @@
-import { Blockchain, SandboxContract, TreasuryContract } from "@ton/sandbox";
-import { Cell, toNano } from "@ton/core";
-import { Mathematics } from "../wrappers/Mathematics";
-import "@ton/test-utils";
-import { compile } from "@ton/blueprint";
+import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
+import { Cell, toNano } from '@ton/core';
+import { Mathematics } from '../wrappers/Mathematics';
+import '@ton/test-utils';
+import { compile } from '@ton/blueprint';
 
-describe("Mathematics", () => {
-  let code: Cell;
+describe('Mathematics', () => {
+    let code: Cell;
 
-  beforeAll(async () => {
-    code = await compile("Mathematics");
-  });
-
-  let blockchain: Blockchain;
-  let deployer: SandboxContract<TreasuryContract>;
-  let mathematics: SandboxContract<Mathematics>;
-
-  beforeEach(async () => {
-    blockchain = await Blockchain.create();
-
-    mathematics = blockchain.openContract(Mathematics.createFromConfig({
-      counter: 0,
-    }, code));
-
-    deployer = await blockchain.treasury("deployer");
-
-    const deployResult = await mathematics.sendDeploy(
-      deployer.getSender(),
-      toNano("0.05"),
-    );
-
-    expect(deployResult.transactions).toHaveTransaction({
-      from: deployer.address,
-      to: mathematics.address,
-      deploy: true,
-      success: true,
-    });
-  });
-
-  it("should deploy", async () => {
-    // the check is done inside beforeEach
-    // blockchain and mathematics are ready to use
-  });
-
-  it("Nth Power: should increase counter", async () => {
-    const increaser = await blockchain.treasury("increaserRandom");
-
-    const counterBefore = await mathematics.getCounter();
-
-    console.log("counter before increasing", counterBefore);
-
-    const increaseBy = Math.floor(Math.random() * 100);
-
-    console.log("increasing by", increaseBy);
-
-    const increaseResult = await mathematics.sendRaiseN(
-      increaser.getSender(),
-      {
-        inputNumber: increaseBy,
-        value: toNano("0.05"),
-      },
-    );
-
-    expect(increaseResult.transactions).toHaveTransaction({
-      from: increaser.address,
-      to: mathematics.address,
-      success: true,
+    beforeAll(async () => {
+        code = await compile('Mathematics');
     });
 
-    const counterAfter = await mathematics.getCounter();
+    let blockchain: Blockchain;
+    let deployer: SandboxContract<TreasuryContract>;
+    let mathematics: SandboxContract<Mathematics>;
 
-    console.log("counter after increasing", counterAfter);
+    beforeEach(async () => {
+        blockchain = await Blockchain.create();
 
-    expect(counterAfter).toBe(counterBefore + increaseBy ** 3);
-  });
+        mathematics = blockchain.openContract(
+            Mathematics.createFromConfig(
+                {
+                    counter: 0,
+                },
+                code,
+            ),
+        );
 
-  it("Random: should increase counter", async () => {
-    const increaser = await blockchain.treasury("increaserRandom");
+        deployer = await blockchain.treasury('deployer');
 
-    const counterBefore = await mathematics.getCounter();
+        const deployResult = await mathematics.sendDeploy(deployer.getSender(), toNano('0.05'));
 
-    console.log("counter before increasing", counterBefore);
-
-    const increaseResult = await mathematics.sendRandom(
-      increaser.getSender(),
-      {
-        value: toNano("0.05"),
-      },
-    );
-
-    expect(increaseResult.transactions).toHaveTransaction({
-      from: increaser.address,
-      to: mathematics.address,
-      success: true,
+        expect(deployResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: mathematics.address,
+            deploy: true,
+            success: true,
+        });
     });
 
-    const counterAfter = await mathematics.getCounter();
-
-    console.log("counter after increasing", counterAfter);
-
-    expect(counterAfter).toBeGreaterThan(counterBefore);
-  });
-
-  it("Current time: set counter to current time", async () => {
-    const increaser = await blockchain.treasury("increaserTime");
-
-    const counterBefore = await mathematics.getCounter();
-
-    console.log("counter before increasing", counterBefore);
-
-    const increaseResult = await mathematics.sendCurrentTime(
-      increaser.getSender(),
-      {
-        value: toNano("0.05"),
-      },
-    );
-
-    expect(increaseResult.transactions).toHaveTransaction({
-      from: increaser.address,
-      to: mathematics.address,
-      success: true,
+    it('should deploy', async () => {
+        // the check is done inside beforeEach
+        // blockchain and mathematics are ready to use
     });
 
-    const counterAfter = Math.floor(Date.now() / 1000);
+    it('Nth Power: should increase counter', async () => {
+        const increaser = await blockchain.treasury('increaserRandom');
 
-    console.log("counter after increasing", counterAfter);
+        const counterBefore = await mathematics.getCounter();
 
-    expect(counterAfter).toBeGreaterThan(counterBefore);
-  });
+        console.log('counter before increasing', counterBefore);
+
+        const increaseBy = Math.floor(Math.random() * 100);
+
+        console.log('increasing by', increaseBy);
+
+        const increaseResult = await mathematics.sendRaiseN(increaser.getSender(), {
+            inputNumber: increaseBy,
+            value: toNano('0.05'),
+        });
+
+        expect(increaseResult.transactions).toHaveTransaction({
+            from: increaser.address,
+            to: mathematics.address,
+            success: true,
+        });
+
+        const counterAfter = await mathematics.getCounter();
+
+        console.log('counter after increasing', counterAfter);
+
+        expect(counterAfter).toBe(counterBefore + increaseBy ** 3);
+    });
+
+    it('Random: should increase counter', async () => {
+        const increaser = await blockchain.treasury('increaserRandom');
+
+        const counterBefore = await mathematics.getCounter();
+
+        console.log('counter before increasing', counterBefore);
+
+        const increaseResult = await mathematics.sendRandom(increaser.getSender(), {
+            value: toNano('0.05'),
+        });
+
+        expect(increaseResult.transactions).toHaveTransaction({
+            from: increaser.address,
+            to: mathematics.address,
+            success: true,
+        });
+
+        const counterAfter = await mathematics.getCounter();
+
+        console.log('counter after increasing', counterAfter);
+
+        expect(counterAfter).toBeGreaterThan(counterBefore);
+    });
+
+    it('Current time: set counter to current time', async () => {
+        const increaser = await blockchain.treasury('increaserTime');
+
+        const counterBefore = await mathematics.getCounter();
+
+        console.log('counter before increasing', counterBefore);
+
+        const increaseResult = await mathematics.sendCurrentTime(increaser.getSender(), {
+            value: toNano('0.05'),
+        });
+
+        expect(increaseResult.transactions).toHaveTransaction({
+            from: increaser.address,
+            to: mathematics.address,
+            success: true,
+        });
+
+        const counterAfter = Math.floor(Date.now() / 1000);
+
+        console.log('counter after increasing', counterAfter);
+
+        expect(counterAfter).toBeGreaterThan(counterBefore);
+    });
+
+    it('Modulo operations: should increase counter', async () => {
+        const increaser = await blockchain.treasury('increaserModulo');
+
+        const counterBefore = await mathematics.getCounter();
+
+        console.log('counter before modulo operation', counterBefore);
+
+        const xp = BigInt('12345678901234567890');
+        const zp = BigInt('98765432109876543210');
+
+        const increaseResult = await mathematics.sendModuloOperations(increaser.getSender(), {
+            xp,
+            zp,
+            value: toNano('0.05'),
+        });
+
+        expect(increaseResult.transactions).toHaveTransaction({
+            from: increaser.address,
+            to: mathematics.address,
+            success: true,
+        });
+
+        const counterAfter = await mathematics.getCounter();
+
+        console.log('counter after modulo operation', counterAfter);
+
+        expect(counterAfter).toBeGreaterThan(counterBefore);
+    });
 });
--- Mathematics/wrappers/Mathematics.ts
+++ Mathematics/wrappers/Mathematics.ts
@@ -1,96 +1,96 @@
-import {
-  Address,
-  beginCell,
-  Cell,
-  Contract,
-  contractAddress,
-  ContractProvider,
-  Sender,
-  SendMode,
-} from "@ton/core";
+import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
 
 export type MathematicsConfig = { counter: number };
 
 export function mathematicsConfigToCell(config: MathematicsConfig): Cell {
-  return beginCell().storeUint(config.counter, 32).endCell();
+    return beginCell().storeUint(config.counter, 32).endCell();
 }
 
 export class Mathematics implements Contract {
-  constructor(
-    readonly address: Address,
-    readonly init?: { code: Cell; data: Cell },
-  ) {}
+    constructor(
+        readonly address: Address,
+        readonly init?: { code: Cell; data: Cell },
+    ) {}
 
-  static createFromAddress(address: Address) {
-    return new Mathematics(address);
-  }
+    static createFromAddress(address: Address) {
+        return new Mathematics(address);
+    }
 
-  static createFromConfig(
-    config: MathematicsConfig,
-    code: Cell,
-    workchain = 0,
-  ) {
-    const data = mathematicsConfigToCell(config);
-    const init = { code, data };
-    return new Mathematics(contractAddress(workchain, init), init);
-  }
+    static createFromConfig(config: MathematicsConfig, code: Cell, workchain = 0) {
+        const data = mathematicsConfigToCell(config);
+        const init = { code, data };
+        return new Mathematics(contractAddress(workchain, init), init);
+    }
 
-  async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
-    await provider.internal(via, {
-      value,
-      sendMode: SendMode.PAY_GAS_SEPARATELY,
-      body: beginCell().endCell(),
-    });
-  }
+    async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
+        await provider.internal(via, {
+            value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().endCell(),
+        });
+    }
 
-  async sendRaiseN(
-    provider: ContractProvider,
-    via: Sender,
-    opts: {
-      inputNumber: number;
-      value: bigint;
-    },
-  ) {
-    await provider.internal(via, {
-      value: opts.value,
-      sendMode: SendMode.PAY_GAS_SEPARATELY,
-      body: beginCell()
-        .storeUint(1, 32)
-        .storeUint(opts.inputNumber, 32)
-        .endCell(),
-    });
-  }
+    async sendRaiseN(
+        provider: ContractProvider,
+        via: Sender,
+        opts: {
+            inputNumber: number;
+            value: bigint;
+        },
+    ) {
+        await provider.internal(via, {
+            value: opts.value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().storeUint(1, 32).storeUint(opts.inputNumber, 32).endCell(),
+        });
+    }
 
-  async sendRandom(
-    provider: ContractProvider,
-    via: Sender,
-    opts: {
-      value: bigint;
-    },
-  ) {
-    await provider.internal(via, {
-      value: opts.value,
-      sendMode: SendMode.PAY_GAS_SEPARATELY,
-      body: beginCell().storeUint(2, 32).storeUint(1, 32).endCell(),
-    });
-  }
+    async sendRandom(
+        provider: ContractProvider,
+        via: Sender,
+        opts: {
+            value: bigint;
+        },
+    ) {
+        await provider.internal(via, {
+            value: opts.value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().storeUint(2, 32).storeUint(1, 32).endCell(),
+        });
+    }
 
-  async sendCurrentTime(
-    provider: ContractProvider,
-    via: Sender,
-    opts: {
-      value: bigint;
-    },
-  ) {
-    await provider.internal(via, {
-      value: opts.value,
-      sendMode: SendMode.PAY_GAS_SEPARATELY,
-      body: beginCell().storeUint(3, 32).storeUint(1, 32).endCell(),
-    });
-  }
+    async sendCurrentTime(
+        provider: ContractProvider,
+        via: Sender,
+        opts: {
+            value: bigint;
+        },
+    ) {
+        await provider.internal(via, {
+            value: opts.value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().storeUint(3, 32).storeUint(1, 32).endCell(),
+        });
+    }
 
-  async getCounter(provider: ContractProvider) {
-    const result = await provider.get("get_counter", []);
-    return result.stack.readNumber();
-  }
+    async sendModuloOperations(
+        provider: ContractProvider,
+        via: Sender,
+        opts: {
+            xp: bigint;
+            zp: bigint;
+            value: bigint;
+        },
+    ) {
+        await provider.internal(via, {
+            value: opts.value,
+            sendMode: SendMode.PAY_GAS_SEPARATELY,
+            body: beginCell().storeUint(4, 32).storeUint(opts.xp, 256).storeUint(opts.zp, 256).endCell(),
+        });
+    }
+
+    async getCounter(provider: ContractProvider) {
+        const result = await provider.get('get_counter', []);
+        return result.stack.readNumber();
+    }
 }
